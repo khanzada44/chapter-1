b1c92a0b5d9baacc78b97d7e5c932aa57f1ee076
khanzada44 <minhamkhanzada4455@gmail.com>
2026-04-06 18:16:11 +0500

    Pratices DSA In JS

--- Master-the-Basics.js
+++ Master-the-Basics.js
@@ -16,5 +16,33 @@ a = a - b // 20
 let divide_a = 7;
 let divide_b = 2;
 
-console.log(Math.floor(divide_a/divide_b));
+// console.log(Math.floor(divide_a/divide_b));
 
+
+let mode_a = 7;
+let mode_b = 2;
+
+// console.log(mode_a % mode_b);
+
+
+
+let d = 4561;
+// console.log(Math.floor(d/10));
+
+
+// < > <= >= != || &&
+
+console.log(10 > 5); // true
+console.log(10 < 5); // false
+console.log(10 >= 10); // true
+console.log(10 <= 9); // false
+console.log(10 != 11); // true
+console.log(10 == 10); // true
+console.log(10 === '10'); // false
+console.log(5 > 3 && 10 < 18.5); //true
+console.log(5 > 30 || 10 < 18.5); //true
+
+
+
+
+// ++ --
