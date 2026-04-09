f43310274b271b7b4dde13574722955add8d1a4a
khanzada44 <minhamkhanzada4455@gmail.com>
2026-04-09 22:05:12 +0500

    prictes the loops

---
+++ Everything-About-Loops.js
@@ -0,0 +1,65 @@
+//  //loop
+
+// for (start; end; change) { }
+
+// // 1-22 Loop
+
+// for (let i = 1; i <= 22 ;i++) {
+//     console.log(i);
+// }
+
+// // 23 to 55 Loop
+
+// for (let i = 23; i <= 55; i++) {
+//     console.log(i);
+// }
+
+// // 200 - 100 Loop
+// for (let i = 200; i >= 100; i--) {
+//     console.log(i);      
+// }
+
+// // Sum of n nutural number 
+
+// let user_number = 12;
+// let sum = 0;
+// for (let i = 0; i <= user_number; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
+// // factorial Number
+
+// let user_number = 5;
+// let factorial = 1;
+// for (let i = 1; i < user_number; i++) {
+//     factorial = factorial * i;
+// }
+// console.log(factorial);
+
+
+// // factor number
+
+// let user_number = 12;
+// for (let i = 0; i <=Math.floor(user_number/2); i++) {
+//     if (user_number % i === 0) {
+//         console.log(i);
+//     }
+// }
+// console.log(user_number);
+
+
+// // prime number True & False
+
+
+// let user_number = 11;
+// let is_prime = true;
+
+// for (let i = 2; i < user_number; i++) {
+//     if (user_number % i === 0) {
+//         is_prime = false;
+//         break;
+//     }
+// }
+// console.log(is_prime ? 'Prime':'Not Prime');
+
